bf0ea03d7cb78caa57bb4b2c71966726752f85e6
Dohyeon Lee <dleh428@kaist.ac.kr>
2021-08-25 10:12:05 +0900

    TCFile: separate raw iter and true RI data iter

--- Tomocell/TCFile.py
+++ Tomocell/TCFile.py
@@ -4,38 +4,51 @@ from typing import List, Union
 from multimethod import multimethod
 
 class TCFile:
-    def __init__(self, TCFname:str, dtype:str):
+    
+
+    class TCFile_raw:
+        def __init__(self, TCFname:str, imgtype:str):
+            self.TCFname=TCFname
+            self.imgtype=imgtype
+        
+        def __getitem__(self, key):
+            if key >= self.len:
+                raise IndexError(f'{TCFile.__name__} index out of range')
+            with h5py.File(self.TCFname) as f:
+                data = f[f'Data/{self.imgtype}/{key:06d}'][()]
+            return data
+
+    def __init__(self, TCFname:str, imgtype:str, dtype=np.float32):
         '''
         object that ease the TCF data manupulation
 
         validation checkstep
-        - dtype validation
+        - imgtype validation
             - only support "2D" and "3D"
-            - check whether if the dtype is supported in the TCFile
+            - check whether if the imgtype is supported in the TCFile
         '''
-        if dtype not in ('2D', '3D'):
-            raise ValueError('dtype only support "2D" and "3D"')
+        if imgtype not in ('2D', '3D'):
+            raise ValueError('imgtype only support "2D" and "3D"')
 
         self.TCFname=TCFname
-        self.dtype=dtype
+        self.imgtype=imgtype
+        self.dtype = dtype
         
         with h5py.File(TCFname) as f:
-            if dtype not in f['Data']:
+            if imgtype not in f['Data']:
                 raise ValueError('The TCFile does not support the suggested file')
-            getAttr = lambda name: f[f'Data/{dtype}'].attrs[name][0]
+            getAttr = lambda name: f[f'Data/{imgtype}'].attrs[name][0]
             self.len = getAttr('DataCount')
             self.shape = tuple(getAttr(f'Size{idx}') for idx in  ('Z', 'Y', 'X'))
             self.resol = tuple(getAttr(f'Resolution{idx}') for idx in  ('Z', 'Y', 'X'))
             self.Volpix = np.prod(self.resol) # (μm)^D (D:dimensions)
             self.dt = 0 if self.len == 1 else getAttr('TimeInterval') # s
-    
-    def __getitem__ (self, key:int) -> np.ndarray:
-        '''
-        '''
-        if key >= self.len:
-            raise IndexError(f'{TCFile.__name__} index out of range')
-        with h5py.File(self.TCFname) as f:
-            data = f[f'Data/{self.dtype}/{key:06d}'][:]
+
+        self.raw = TCFile.TCFile_raw(TCFname,imgtype)
+
+    def __getitem__ (self, key) -> np.ndarray:
+        data = self.raw[key].astype(self.dtype)
+        data /= 1e4
         return data
 
     def __len__(self) -> int:
